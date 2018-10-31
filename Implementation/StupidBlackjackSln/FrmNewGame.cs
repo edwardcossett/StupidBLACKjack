3b2fda6fdb8c463e400fcea10c9e4c833172af41
jimwallace <jwallace@fenwaygroup.com>
2018-10-30 19:03:08 -0500

    Added Dealer and Win State

--- Implementation/StupidBlackjackSln/FrmNewGame.cs
+++ Implementation/StupidBlackjackSln/FrmNewGame.cs
@@ -14,27 +14,40 @@ namespace StupidBlackjackSln {
   public partial class FrmNewGame : Form {
     private Deck deck;
     private Player player1;
+    private Dealer dealer1;
     private PictureBox[] picPlayerCards;
+    private PictureBox[] picDealerCards;
 
     public FrmNewGame() {
       InitializeComponent();
       picPlayerCards = new PictureBox[5];
+      picDealerCards = new PictureBox[5];
       for (int i = 0; i < 5; i++) {
+        Console.WriteLine(i);
         picPlayerCards[i] = Controls.Find("picPlayerCard" + (i + 1).ToString(), true)[0] as PictureBox;
+      }
+      for (int i = 0; i < 5; i++) {
+        Console.WriteLine(i);
+        picDealerCards[i] = Controls.Find("picDealerCard" + (i + 1).ToString(), true)[0] as PictureBox;
       }
     }
 
     private void FrmNewGame_Load(object sender, EventArgs e) {
       deck = new Deck(FindBitmap);
       player1 = new BlackjackPlayer();
+      dealer1 = new BlackjackDealer();
       
       player1.giveHand(new List<Card>() { deck.dealCard(), deck.dealCard() });
+      dealer1.giveHand(new List<Card>() { deck.dealCard(), deck.dealCard() });
       showHand();
     }
 
     private void showHand() {
       for (int i = 0; i < player1.Hand.Count(); i++) {
         picPlayerCards[i].BackgroundImage = player1.Hand[i].Bitmap;
+      }
+      for (int i = 0; i < dealer1.Hand.Count(); i++) {
+        picDealerCards[i].BackgroundImage = dealer1.Hand[i].Bitmap;
       }
       lblPlayerScore.Text = player1.Score.ToString();
     }
@@ -48,6 +61,7 @@ namespace StupidBlackjackSln {
     private void btnHit_Click(object sender, EventArgs e) {
       player1.giveCard(deck.dealCard());
       showHand();
+            System.Threading.Thread.Sleep(200);
             if (player1.Score > 21)
             {
                 FrmLose frmLose = new FrmLose();
@@ -56,9 +70,43 @@ namespace StupidBlackjackSln {
             }
     }
 
+    private void btnStand_Click(object sender, EventArgs e)
+    {
+            while (dealer1.Score<player1.Score && dealer1.Score < 17)
+            {
+                dealer1.giveCard(deck.dealCard());
+                showHand();
+                System.Threading.Thread.Sleep(500);
+                
+            }
+            if (dealer1.Score > 21)
+            {
+                FrmWin frmWin = new FrmWin();
+                frmWin.Show();
+                this.Hide();
+            }
+            if (player1.Score <= dealer1.Score)
+            {
+                FrmLose frmLose = new FrmLose();
+                frmLose.Show();
+                this.Hide();
+            }
+            else
+            {
+                FrmWin frmWin = new FrmWin();
+                frmWin.Show();
+                this.Hide();
+            }
+        }
+
     private Bitmap FindBitmap(string value, string suit) {
       string textName = "";
       int valueAsNum;
+            if (value == "back_of_card")
+            {
+                return (Bitmap)Resources.ResourceManager.GetObject(value);
+
+            }
       if (int.TryParse(value, out valueAsNum)) {
         textName += "_";
       }
---
+++ Implementation/StupidBlackjackSln/FrmWin.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace StupidBlackjackSln
+{
+    partial class FrmWin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnTitle = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewGame.Location = new System.Drawing.Point(13, 289);
+            this.btnNewGame.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(263, 74);
+            this.btnNewGame.TabIndex = 2;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // btnTitle
+            // 
+            this.btnTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTitle.Location = new System.Drawing.Point(524, 289);
+            this.btnTitle.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTitle.Name = "btnTitle";
+            this.btnTitle.Size = new System.Drawing.Size(263, 74);
+            this.btnTitle.TabIndex = 3;
+            this.btnTitle.Text = "Quit";
+            this.btnTitle.UseVisualStyleBackColor = true;
+            this.btnTitle.Click += new System.EventHandler(this.btnTitle_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Black;
+            this.lblTitle.Location = new System.Drawing.Point(250, 79);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(345, 91);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "You Win";
+            this.lblTitle.Click += new System.EventHandler(this.lblTitle_Click);
+            // 
+            // FrmWin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnTitle);
+            this.Controls.Add(this.btnNewGame);
+            this.Name = "FrmWin";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnTitle;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
