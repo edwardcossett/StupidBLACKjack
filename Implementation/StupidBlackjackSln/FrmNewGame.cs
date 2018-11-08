0da77d147129435f2315557f7a20b811499c3a99
mlasal2 <mattfl007@yahoo.com>
2018-11-07 18:47:55 -0600

    Added dealer 5 card charlie, and blackjack
    Added dealer and player blackjack as well as dealer 5 card charlie

--- Implementation/StupidBlackjackSln/FrmNewGame.cs
+++ Implementation/StupidBlackjackSln/FrmNewGame.cs
@@ -13,70 +13,78 @@ using System.Windows.Forms;
 namespace StupidBlackjackSln
 {
     public partial class FrmNewGame : Form {
-    private Deck deck;
-    private BlackjackPlayer player;
-    private Dealer dealer;
-    private PictureBox[] picPlayerCards;
-    private PictureBox[] picDealerCards;
-    private int streakCounter;
-    private bool DealerTurn;
+        private Deck deck;
+        private BlackjackPlayer player;
+        private Dealer dealer;
+        private PictureBox[] picPlayerCards;
+        private PictureBox[] picDealerCards;
+        private int streakCounter;
+        private bool DealerTurn;
 
-    public Dealer Dealer { get => dealer; set => dealer = value; }
-    public int StreakCounter { get => streakCounter; set => streakCounter = value; }
+        public Dealer Dealer { get => dealer; set => dealer = value; }
+        public int StreakCounter { get => streakCounter; set => streakCounter = value; }
 
-    public FrmNewGame(string playerName) {
-      InitializeComponent();
+        public FrmNewGame(string playerName)
+        {
+            InitializeComponent();
 
-      picPlayerCards = new PictureBox[5];
-      picDealerCards = new PictureBox[5];
+            picPlayerCards = new PictureBox[5];
+            picDealerCards = new PictureBox[5];
 
-      player = new BlackjackPlayer();
-      lblPlayerName.Text = playerName;
-      streakCounter = 0;
+            player = new BlackjackPlayer();
+            lblPlayerName.Text = playerName;
+            streakCounter = 0;
 
-      player.SetName(playerName);
-      
-      dealer = new BlackjackDealer();
+            player.SetName(playerName);
+            
+            dealer = new BlackjackDealer();
 
-      for (int i = 0; i < 5; i++) {
-        Console.WriteLine(i);
-        picPlayerCards[i] = Controls.Find("picPlayerCard" + (i + 1).ToString(), true)[0] as PictureBox;
-      }
-      for (int i = 0; i < 5; i++) {
-        Console.WriteLine(i);
-        picDealerCards[i] = Controls.Find("picDealerCard" + (i + 1).ToString(), true)[0] as PictureBox;
-      }
-    }
-
-    private void FrmNewGame_Load(object sender, EventArgs e) {
-      deck = new Deck(FindBitmap);
-
-      lblPlayerStreak.Text = streakCounter.ToString();
-      player.giveHand(new List<Card>() { deck.dealCard(), deck.dealCard() });
-      dealer.giveHand(new List<Card>() { deck.dealCard(), deck.dealCard() });
-      showHand();
-		}
-
-    private void showHand() {
-      SoundPlayer flip = new SoundPlayer(Resources.CardFlip);
-      SoundPlayer winning = new SoundPlayer(Resources.WinSound);
-      for (int i = 0; i < player.Hand.Count(); i++) {
-        picPlayerCards[i].BackgroundImage = player.Hand[i].Bitmap;
-        flip.Play();
-      }
-      for (int i = 0; i < dealer.Hand.Count(); i++) {
-        picDealerCards[i].BackgroundImage = dealer.Hand[i].Bitmap;
-        if (!DealerTurn && i == 1){
-            picDealerCards[i].BackgroundImage = (Bitmap)Resources.back_of_card;
-            flip.Play();
+            for (int i = 0; i < 5; i++)
+            {
+              Console.WriteLine(i);
+              picPlayerCards[i] = Controls.Find("picPlayerCard" + (i + 1).ToString(), true)[0] as PictureBox;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+              Console.WriteLine(i);
+              picDealerCards[i] = Controls.Find("picDealerCard" + (i + 1).ToString(), true)[0] as PictureBox;
+            }
         }
-      }
-      lblPlayerScore.Text = player.Score.ToString();
-      if(player.Hand.Count()==5 && player.Score <= 21)
+
+        private void FrmNewGame_Load(object sender, EventArgs e)
+        {
+            deck = new Deck(FindBitmap);
+
+            lblPlayerStreak.Text = streakCounter.ToString();
+            player.giveHand(new List<Card>() { deck.dealCard(), deck.dealCard() });
+            dealer.giveHand(new List<Card>() { deck.dealCard(), deck.dealCard() });
+            showHand();
+	    }
+
+        private void showHand()
+        {
+            SoundPlayer flip = new SoundPlayer(Resources.CardFlip);
+            SoundPlayer winning = new SoundPlayer(Resources.WinSound);
+            for (int i = 0; i < player.Hand.Count(); i++)
+            {
+                picPlayerCards[i].BackgroundImage = player.Hand[i].Bitmap;
+                flip.Play();
+            }
+            for (int i = 0; i < dealer.Hand.Count(); i++)
+            {
+                picDealerCards[i].BackgroundImage = dealer.Hand[i].Bitmap;
+                if (!DealerTurn && i == 1)
+                {
+                    picDealerCards[i].BackgroundImage = (Bitmap)Resources.back_of_card;
+                    flip.Play();
+                }
+            }
+            lblPlayerScore.Text = player.Score.ToString();
+            if(player.Hand.Count()==5 && player.Score <= 21)
             {
 				streakCounter += 1;
 				winning.Play();
-                DialogResult result = MessageBox.Show("You Win! Start New Game?", "You Win!", MessageBoxButtons.YesNo);
+                DialogResult result = MessageBox.Show("You Win! Start New Game?", "Five Card Charlie!", MessageBoxButtons.YesNo);
                 if (result == DialogResult.Yes)
                 {
                     startNewGame();
@@ -84,20 +92,64 @@ namespace StupidBlackjackSln
                 else
                 {
                     lossRoutines();
-					//frmTitle frmTitle = new frmTitle();
-                    //frmTitle.Show();
-                    this.Close();
+					frmTitle frmTitle = new frmTitle();
+                    frmTitle.Show();
+                    this.Hide();
                 }
             }
-    }
+            if (player.Hand.Count() == 2 && player.Score == 21)
+            {
+                streakCounter += 1;
+                winning.Play();
+                DialogResult result = MessageBox.Show("Blackjack! Start New Game?", "Blackjack!", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    startNewGame();
+                }
+                else
+                {
+                    lossRoutines();
+                    frmTitle frmTitle = new frmTitle();
+                    frmTitle.Show();
+                    this.Hide();
+                }
+            }
+            if (dealer.Hand.Count() == 2 && dealer.Score == 21)
+            {
+                SoundPlayer losing = new SoundPlayer(Resources.LoseSound);
+                lossRoutines();
+                DealerTurn = true;
+                showHand();
+                losing.Play();
+                DialogResult result = MessageBox.Show("You Lose! Start New Game?", "Dealer Blackjack!", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    startNewGame();
+                }
+                else
+                {
+                    frmTitle frmTitle = new frmTitle();
+                    frmTitle.Show();
+                    this.Hide();
+                }
+            }
+        }
 
+        private void FrmNewGame_FormClosed(object sender, FormClosedEventArgs e)
+            {
+		    foreach (Form f in Application.OpenForms)
+            {
+			    f.Close();
+		    } 
+	    }
 
-    private void btnHit_Click(object sender, EventArgs e) {
-      SoundPlayer losing = new SoundPlayer(Resources.LoseSound);
-      player.giveCard(deck.dealCard());
-      showHand();
+        private void btnHit_Click(object sender, EventArgs e)
+        {
+            player.giveCard(deck.dealCard());
+            showHand();
             if (player.Score > 21)
             {
+                SoundPlayer losing = new SoundPlayer(Resources.LoseSound);
                 lossRoutines();
                 DealerTurn = true;
                 showHand();
@@ -109,24 +161,44 @@ namespace StupidBlackjackSln
                 }
                 else
                 {
-                    //frmTitle frmTitle = new frmTitle();
-                    //frmTitle.Show();
-                    
-                    this.Close();
+                    frmTitle frmTitle = new frmTitle();
+                    frmTitle.Show();
+                    this.Hide();
                 }
             }
-    }
+        }
 
-    private void btnStand_Click(object sender, EventArgs e)
-    {
+        private void btnStand_Click(object sender, EventArgs e)
+        {
             SoundPlayer winning = new SoundPlayer(Resources.WinSound);
             SoundPlayer losing = new SoundPlayer(Resources.LoseSound);
             DealerTurn = true;
             showHand();
             while (dealer.Score<player.Score || dealer.Score < 17)
             {
-                dealer.giveCard(deck.dealCard());
-                showHand();
+                if (dealer.Hand.Count() == 5 && dealer.Score <= 21)
+                {
+                    lossRoutines();
+                    DealerTurn = true;
+                    showHand();
+                    losing.Play();
+                    DialogResult result = MessageBox.Show("You Lose! Start New Game?", "Dealer Five Card Charlie!", MessageBoxButtons.YesNo);
+                    if (result == DialogResult.Yes)
+                    {
+                        startNewGame();
+                    }
+                    else
+                    {
+                        frmTitle frmTitle = new frmTitle();
+                        frmTitle.Show();
+                        this.Hide();
+                    }
+                }
+                else
+                {
+                    dealer.giveCard(deck.dealCard());
+                    showHand();
+                }
                 
             }
             if (dealer.Score > 21)
@@ -141,9 +213,9 @@ namespace StupidBlackjackSln
                 else
                 {
                     lossRoutines();
-					//frmTitle frmTitle = new frmTitle();
-                    //frmTitle.Show();
-                    this.Close();
+					frmTitle frmTitle = new frmTitle();
+                    frmTitle.Show();
+                    this.Hide();
                 }
             }
             else if (player.Score < dealer.Score)
@@ -158,9 +230,9 @@ namespace StupidBlackjackSln
                 }
                 else
                 {
-                    //frmTitle frmTitle = new frmTitle();
-                    //frmTitle.Show();
-                    this.Close();
+                    frmTitle frmTitle = new frmTitle();
+                    frmTitle.Show();
+                    this.Hide();
                 }
             }
             else if (player.Score == dealer.Score)
@@ -172,9 +244,9 @@ namespace StupidBlackjackSln
                 }
                 else
                 {
-                    //frmTitle frmTitle = new frmTitle();
-                    //frmTitle.Show();
-                    this.Close();
+                    frmTitle frmTitle = new frmTitle();
+                    frmTitle.Show();
+                    this.Hide();
                 }
             }
             else
@@ -189,23 +261,14 @@ namespace StupidBlackjackSln
                 else
                 {
                     lossRoutines();
-					//frmTitle frmTitle = new frmTitle();
-                    //frmTitle.Show();
-                    this.Close();
+					frmTitle frmTitle = new frmTitle();
+                    frmTitle.Show();
+                    this.Hide();
                 }
             }
         }
 
-    /// <summary>
-    /// This function manages storing player scores when appropriate
-    /// 
-    /// Scores are read from the input.txt file and placed in the output.txt
-    /// file until the current player is found. When the current player's name
-    /// is found, the previous high score is compared and the new high score
-    /// is written to output.txt. After all other scores are written to
-    /// output.txt, input.txt is deleted and output.txt is renamed input.txt.
-    /// </summary>
-    private void lossRoutines()
+        private void lossRoutines()
         {
             if(!File.Exists("..\\..\\Resources\\input.txt"))
 			{
@@ -242,7 +305,7 @@ namespace StupidBlackjackSln
             streakCounter = 0;
         }
 
-    private void startNewGame()
+        private void startNewGame()
         {
             DealerTurn = false;
             player.Hand.Clear();
@@ -266,42 +329,30 @@ namespace StupidBlackjackSln
             
         }
 
-    private Bitmap FindBitmap(string value, string suit) {
-      string textName = "";
-      int valueAsNum;
+        private Bitmap FindBitmap(string value, string suit)
+        {
+            string textName = "";
+            int valueAsNum;
             if (value == "back_of_card")
             {
                 return (Bitmap)Resources.ResourceManager.GetObject(value);
 
             }
-      if (int.TryParse(value, out valueAsNum)) {
-        textName += "_";
-      }
+            if (int.TryParse(value, out valueAsNum))
+            {
+                textName += "_";
+            }
 
-      textName += value;
-      textName += "_of_";
-      textName += suit;
+            textName += value;
+            textName += "_of_";
+            textName += suit;
 
-      return (Bitmap)Resources.ResourceManager.GetObject(textName);
-    }
+            return (Bitmap)Resources.ResourceManager.GetObject(textName);
+        }
 
         private void lblPlayerName_Click(object sender, EventArgs e)
         {
 
-        }
-
-        /// <summary>
-        /// Used to reopen title after FrmNewGame is closed. If many random forms
-        /// start opening, refine this function to only open the title.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void FrmNewGame_FormClosed(object sender, FormClosedEventArgs e)
-        {
-            foreach (Form f in Application.OpenForms)
-            {
-                f.Show();
-            }
         }
     }
 }
